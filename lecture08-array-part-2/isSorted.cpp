539a999a1939c463591464238567322b0aaa0f79
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-12-18 14:07:33 +0530

    Add isSorted function to check if an array is sorted

---
+++ lecture08-array-part-2/isSorted.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSorted(vector<int> &arr)
+    {
+        // code here
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i - 1] > arr[i])
+                return false;
+        }
+        return true;
+    }
+};
