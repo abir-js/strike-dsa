0e8acdc2863b2142ca9f944aeb91a3e71d7f1203
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-12-22 17:46:57 +0530

    Add second implementation of sumOfUnique function using a hash map for improved efficiency

--- lecture09-array-part-3/sumOfUniqueElements.cpp
+++ lecture09-array-part-3/sumOfUniqueElements.cpp
@@ -22,3 +22,24 @@ public:
         return sum;
     }
 };
+
+// Method 2: Using Hash Map
+
+class Solution {
+public:
+	int sumOfUnique(vector<int>& nums) {
+		int n = nums.size();
+		int sum = 0;
+		vector<int>freq(101, 0);
+		for(int i=0; i<n; i++){
+			freq[nums[i]]++;
+		}
+		
+		for(int i=1; i<freq.size(); i++){
+			if(freq[i] == 1){
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+};
