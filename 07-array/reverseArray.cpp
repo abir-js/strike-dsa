4bf4432fe5175b2fbf46964172047455061eee31
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-03-16 09:57:26 +0530

    added reverse array code

---
+++ 07-array/reverseArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+
+    int arr [5] = {20, 13, 35, 12, 50};
+
+    int size = sizeof(arr)/sizeof(int);
+
+    cout << size << endl;
+
+ 
+
+    return 0;
+}
