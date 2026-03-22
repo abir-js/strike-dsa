8d2aaa4e0f9c016ac406ff79c9d4b54f57a58829
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-03-23 00:25:52 +0530

    Add implementation for adding a new element after a target node in linked list

---
+++ 39-linkedlist/addNewElementAfterTargetNode.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    int arr[] = {10, 30, 70, 20, 15, 25};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!head)
+        {
+            head = new Node(arr[i]);
+            tail = head;
+        }
+        else
+        {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+    }
+
+    // *  Current Linked List
+    // 10 30 70 20 15 25
+
+    // Create and insert node
+    Node *newNode = new Node(80);
+
+    //* add after 20;
+    Node *temp = head;
+    while (temp->data != 20)
+    {
+        temp = temp->next;
+    }
+    if (temp->data == 20)
+    {
+        Node *elementAfterTarget = temp->next;
+        newNode->next = elementAfterTarget;
+        temp->next = newNode;
+    }
+
+    // print LL --> 10 30 70 20 80 15 25 
+    temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
