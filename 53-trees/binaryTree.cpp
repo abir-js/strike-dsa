d22a58421ee2503ad3c44c6e4aad7794d1320607
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-04-22 11:28:30 +0530

    Refactor binary tree traversal methods and enhance user interaction in main function

--- 53-trees/binaryTree.cpp
+++ 53-trees/binaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class Node
@@ -6,23 +7,53 @@ class Node
 public:
     int data;
     Node *left, *right;
+
     Node(int data)
     {
         this->data = data;
         left = right = NULL;
     }
+
+    void preOrderTraversal(Node *root)
+    {
+        if (!root)
+            return;
+
+        cout << root->data << " ";
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+
+    void inOrderTraversal(Node *root)
+    {
+        if (!root)
+            return;
+
+        inOrderTraversal(root->left);
+        cout << root->data << " ";
+        inOrderTraversal(root->right);
+    }
+
+    void postOrderTraversal(Node *root)
+    {
+        if (!root)
+            return;
+
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        cout << root->data << " ";
+    }
 };
 
 int main()
 {
-
     int x, leftValue, rightValue;
     cout << "Enter root node value: ";
     cin >> x;
+
     Node *rootNode = new Node(x);
 
     queue<Node *> q;
-
     q.push(rootNode);
 
     while (!q.empty())
@@ -32,6 +63,7 @@ int main()
 
         cout << "Enter " << temp->data << "'s left node value: ";
         cin >> leftValue;
+
         if (leftValue != -1)
         {
             temp->left = new Node(leftValue);
@@ -40,6 +72,7 @@ int main()
 
         cout << "Enter " << temp->data << "'s right node value: ";
         cin >> rightValue;
+
         if (rightValue != -1)
         {
             temp->right = new Node(rightValue);
@@ -47,5 +80,39 @@ int main()
         }
     }
 
-    return 0;
+    int choice;
+
+    while (true)
+    {
+        cout << "\n1. In-order\n2. Post-order\n3. Pre-order\n4. Exit\nEnter choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "In-order Traversal: ";
+            rootNode->inOrderTraversal(rootNode);
+            cout << endl;
+            break;
+
+        case 2:
+            cout << "Post-order Traversal: ";
+            rootNode->postOrderTraversal(rootNode);
+            cout << endl;
+            break;
+
+        case 3:
+            cout << "Pre-order Traversal: ";
+            rootNode->preOrderTraversal(rootNode);
+            cout << endl;
+            break;
+
+        case 4:
+            cout << "Exiting...\n";
+            return 0;
+
+        default:
+            cout << "Invalid choice!\n";
+        }
+    }
 }
